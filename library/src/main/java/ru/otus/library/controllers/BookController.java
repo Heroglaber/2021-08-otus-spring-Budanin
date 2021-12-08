c78dbe5beedc5157ec66cd54375045d0f2f0c4f8
abudanin <alexander.budanin@gmail.com>
2021-12-08 14:28:56 +0300

    Redesigned book controller to rest

--- library/src/main/java/ru/otus/library/controllers/BookController.java
+++ library/src/main/java/ru/otus/library/controllers/BookController.java
@@ -13,101 +13,129 @@ import ru.otus.library.services.BookService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping(value = {"/", "/library"})
 public class BookController {
     private final BookService bookService;
 
-    @GetMapping
-    public String getAll(Model model) {
-        List<BookDTO> books = bookService.getAll();
-        model.addAttribute("books", books);
-        return "list";
+    @GetMapping("/book")
+    public List<BookDTO> getAll() {
+        return bookService.getAll();
     }
 
-    @GetMapping("/addBook")
-    public String addForm(Model model) {
-        BookDTO book = new BookDTO();
-        book.addAuthor(new AuthorDTO());
-        book.addGenre(new GenreDTO());
-        model.addAttribute("book", book);
-        return "add_form";
+    @GetMapping("/book/{bookId}")
+    public BookDTO getBook(@PathVariable("bookId") String bookId) {
+        try {
+            return bookService.getById(bookId);
+        }
+        catch(NoSuchElementException e) {
+            return null;
+        }
     }
 
-    @PostMapping("/add")
-    public String addBook(@ModelAttribute("book") BookDTO book) {
+    @PostMapping("/book")
+    public BookDTO addBook(@RequestBody BookDTO book) {
         book.getAuthors().removeIf(authorDTO -> authorDTO.getName() == null);
         book.getGenres().removeIf(genreDTO -> genreDTO.getName() == null);
-        bookService.add(book);
-        return "redirect:/library";
+        return bookService.add(book);
     }
 
-    @PostMapping(value="/addBookAuthor", params={"viewName", "addBookAuthor"})
-    public  String addBookAuthor(@ModelAttribute("book") BookDTO book,
-                                 Model model,
-                                 HttpServletRequest req) {
-        final String viewName = req.getParameter("viewName");
-        book.addAuthor(new AuthorDTO());
-        return viewName + " :: authors";
-    }
-
-    @PostMapping(value="/removeBookAuthor", params={"viewName", "removeBookAuthor"})
-    public String removeBookAuthor(
-            @ModelAttribute("book") BookDTO book,
-            HttpServletRequest req) {
-        final String viewName = req.getParameter("viewName");
-        final Integer authorFieldId = Integer.valueOf(req.getParameter("removeBookAuthor"));
-        book.getAuthors().remove(authorFieldId.intValue());
-        return viewName + " :: authors";
-    }
-
-    @PostMapping(value="/addBookGenre", params={"viewName", "addBookGenre"})
-    public  String addBookGenre(@ModelAttribute("book") BookDTO book,
-                                Model model,
-                                HttpServletRequest req) {
-        final String viewName = req.getParameter("viewName");
-        book.addGenre(new GenreDTO());
-        return viewName + " :: genres";
-    }
-
-    @PostMapping(value="/removeBookGenre", params={"viewName", "removeBookGenre"})
-    public String removeBookGenre(
-            @ModelAttribute("book") BookDTO book,
-            HttpServletRequest req) {
-        final Integer genreFieldId = Integer.valueOf(req.getParameter("removeBookGenre"));
-        book.getGenres().remove(genreFieldId.intValue());
-        final String viewName = req.getParameter("viewName");
-        return viewName + " :: genres";
-    }
-
-    @DeleteMapping("/delete/{bookId}")
-    public String deleteBook(@PathVariable("bookId") String bookId) {
-        bookService.deleteById(bookId);
-        return "redirect:/library";
-    }
-
-    @GetMapping("/edit/{bookId}")
-    public String editForm(@PathVariable("bookId") String bookId, Model model) {
-        BookDTO book = bookService.getById(bookId);
-        model.addAttribute("book", book);
-        return "edit_form";
-    }
-
-    @PostMapping("/edit/{bookId}")
-    public String editBook(@PathVariable("bookId") String bookId, @ModelAttribute("book") BookDTO book) {
+    @PostMapping("/book/{bookId}")
+    public BookDTO editBook(@RequestBody BookDTO book, @PathVariable("bookId") String bookId) {
         if(!bookId.equals(book.getId())) {
             throw new RuntimeException("Book id in url not match id in model.");
         }
         book.getAuthors().removeIf(authorDTO -> authorDTO.getName() == null);
         book.getGenres().removeIf(genreDTO -> genreDTO.getName() == null);
-        bookService.update(book);
-        return "redirect:/library";
+        return bookService.update(book);
     }
 
-    @InitBinder    /* Converts empty strings into null when a form is submitted */
-    public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    @DeleteMapping("/book/{bookId}")
+    public BookDTO deleteBook(@PathVariable("bookId") String bookId) {
+        try {
+            return bookService.deleteById(bookId);
+        }
+        catch(NoSuchElementException e) {
+            return null;
+        }
     }
+
+//    @GetMapping("/addBook")
+//    public String addForm(Model model) {
+//        BookDTO book = new BookDTO();
+//        book.addAuthor(new AuthorDTO());
+//        book.addGenre(new GenreDTO());
+//        model.addAttribute("book", book);
+//        return "add_form";
+//    }
+//
+//    @PostMapping(value="/addBookAuthor", params={"viewName", "addBookAuthor"})
+//    public  String addBookAuthor(@ModelAttribute("book") BookDTO book,
+//                                 Model model,
+//                                 HttpServletRequest req) {
+//        final String viewName = req.getParameter("viewName");
+//        book.addAuthor(new AuthorDTO());
+//        return viewName + " :: authors";
+//    }
+//
+//    @PostMapping(value="/removeBookAuthor", params={"viewName", "removeBookAuthor"})
+//    public String removeBookAuthor(
+//            @ModelAttribute("book") BookDTO book,
+//            HttpServletRequest req) {
+//        final String viewName = req.getParameter("viewName");
+//        final Integer authorFieldId = Integer.valueOf(req.getParameter("removeBookAuthor"));
+//        book.getAuthors().remove(authorFieldId.intValue());
+//        return viewName + " :: authors";
+//    }
+//
+//    @PostMapping(value="/addBookGenre", params={"viewName", "addBookGenre"})
+//    public  String addBookGenre(@ModelAttribute("book") BookDTO book,
+//                                Model model,
+//                                HttpServletRequest req) {
+//        final String viewName = req.getParameter("viewName");
+//        book.addGenre(new GenreDTO());
+//        return viewName + " :: genres";
+//    }
+//
+//    @PostMapping(value="/removeBookGenre", params={"viewName", "removeBookGenre"})
+//    public String removeBookGenre(
+//            @ModelAttribute("book") BookDTO book,
+//            HttpServletRequest req) {
+//        final Integer genreFieldId = Integer.valueOf(req.getParameter("removeBookGenre"));
+//        book.getGenres().remove(genreFieldId.intValue());
+//        final String viewName = req.getParameter("viewName");
+//        return viewName + " :: genres";
+//    }
+//
+//    @DeleteMapping("/delete/{bookId}")
+//    public String deleteBook(@PathVariable("bookId") String bookId) {
+//        bookService.deleteById(bookId);
+//        return "redirect:/library";
+//    }
+//
+//    @GetMapping("/edit/{bookId}")
+//    public String editForm(@PathVariable("bookId") String bookId, Model model) {
+//        BookDTO book = bookService.getById(bookId);
+//        model.addAttribute("book", book);
+//        return "edit_form";
+//    }
+//
+//    @PostMapping("/edit/{bookId}")
+//    public String editBook(@PathVariable("bookId") String bookId, @ModelAttribute("book") BookDTO book) {
+//        if(!bookId.equals(book.getId())) {
+//            throw new RuntimeException("Book id in url not match id in model.");
+//        }
+//        book.getAuthors().removeIf(authorDTO -> authorDTO.getName() == null);
+//        book.getGenres().removeIf(genreDTO -> genreDTO.getName() == null);
+//        bookService.update(book);
+//        return "redirect:/library";
+//    }
+//
+//    @InitBinder    /* Converts empty strings into null when a form is submitted */
+//    public void initBinder(WebDataBinder binder) {
+//        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+//    }
 }
